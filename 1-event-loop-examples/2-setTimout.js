cc43de998bf817bf0fe8934ecf118f06ba71f9fc
Harsh Patel <invinciblengo@Invincibles-MacBook-Air.local>
2022-08-16 10:11:04 +0530

    added 2-async-pattern

---
+++ 1-event-loop-examples/2-setTimout.js
@@ -0,0 +1,7 @@
+//Started the operating system process
+console.log('First');
+setTimeout(()=>{
+    console.log('Second');
+},0)
+console.log('third');
+//completed and exited operating system process
